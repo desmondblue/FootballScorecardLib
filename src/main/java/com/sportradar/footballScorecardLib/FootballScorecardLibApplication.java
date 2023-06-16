4113c36d36b094541363f1e559688990032b7f06
Ankush Kalsi <ankushk456@gmail.com>
2023-06-16 16:36:51 +0200

    Initial Commit

---
+++ src/main/java/com/sportradar/footballScorecardLib/FootballScorecardLibApplication.java
@@ -0,0 +1,13 @@
+package com.sportradar.footballScorecardLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballScorecardLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballScorecardLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportradar/footballScorecardLib/FootballScorecardLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportradar.footballScorecardLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballScorecardLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
